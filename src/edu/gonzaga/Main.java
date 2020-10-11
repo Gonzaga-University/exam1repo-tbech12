09af2c5681e3cd8bb60cb82c206534219e6bf8ea
Aaron S. Crandall <acrandal@wsu.edu>
2020-10-11 13:14:17 -0700

    Stubbed in Java version of feature 1 - with adder

---
+++ src/edu/gonzaga/Main.java
@@ -0,0 +1,19 @@
+package edu.gonzaga;
+
+class Adder {
+    int val1;
+    int val2;
+
+    public Adder(int new_val1, int new_val2) {
+        val1 = new_val1;
+        val2 = new_val2;
+    }
+
+
+}
+
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Hello world");
+    }
+}
